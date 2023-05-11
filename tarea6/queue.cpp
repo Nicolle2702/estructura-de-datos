8883314e3fb0f6e9c000e1d33fd486791a3d7bfe
Nicolle2702 <124204699+Nicolle2702@users.noreply.github.com>
2023-05-11 08:29:30 -0500

    Add files via upload

---
+++ tarea6/queue.cpp
@@ -0,0 +1,77 @@
+
+//NICOLLE RUIZ QUINTERO CODIGO: 8974036
+
+
+
+#include<iostream>
+#include<list>
+#include<deque>
+
+using namespace std;
+
+int buscaNum(deque<int>& lista, int pos){
+    int i = 0;
+    int c = -1;
+    for (deque<int>::iterator it=lista.begin();it!=lista.end();it++){
+        i++;
+        if (*it==pos){
+            c=i;
+        }
+    }
+    return c;
+}
+
+int main(){
+    int numpaci; 
+    int numacci;
+    cin>>numpaci;
+    cin>>numacci;
+    int j=1;
+    while(numpaci!=0 && numacci!=0){
+        deque<int> pacien;
+        deque<int> fin;
+        int veces;
+        if(numacci>numpaci){
+            veces=numpaci;
+        }
+        else if(numacci<numpaci){
+            veces=numacci;
+        }
+        else if(numacci==numpaci){
+            veces=numpaci;
+        }
+        for (int i=1;i<=veces;i++) {
+            pacien.push_back(i);
+        }
+        for (int o=1;o<=numacci;o++) {
+            char que;
+            cin>>que;
+            if (que=='N') {
+                pacien.push_back(pacien.front());
+                fin.push_back(pacien.front());
+                pacien.pop_front();
+            } 
+            else if(que=='E') {
+                int numm;
+                cin>>numm;
+                if(numm>numacci){
+                    pacien.push_front(numm);
+                }
+                else{
+                    int c=buscaNum(pacien,numm);
+                    pacien.erase(pacien.begin() + c - 1);
+                    pacien.push_front(numm);
+                }
+            }
+        }
+        cout<<"Case "<<j<<":"<< endl;
+        for (deque<int>::iterator it=fin.begin();it!=fin.end();it++) {
+            cout <<*it << endl;
+        }
+        cin>>numpaci;
+        cin>>numacci;
+        j=j+1;
+    }
+    return 0;
+}
+
