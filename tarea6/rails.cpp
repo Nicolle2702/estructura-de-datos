17bef599823c1b8e15890b06a18448567fe2c3ba
Nicolle2702 <124204699+Nicolle2702@users.noreply.github.com>
2023-05-14 18:30:16 -0500

    Add files via upload

--- tarea6/rails.cpp
+++ tarea6/rails.cpp
@@ -1,78 +1,91 @@
-#include<iostream>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<deque>
 
+//NICOLLE RUIZ QUINTERO CODIGO: 8974036
+
+/* LA COMPLEJIDAD DE ESTE CODIGO ES DE O(N) YA QUE EN EL PEOR DE LOS CASOS TIENE QUE RECORRER SOLO 1 VEZ TODO EL CICLO 
+*/
+
+
+#include<iostream>
+#include<stack>
+#include<queue>
 using namespace std;
 
-int main(){
+int main() {
     stack<int> de1aN; // guarda de 1 a N
     stack<int> acomodo; // Guarda lo que el usuario coloca
-    stack<int> predeterminado; // guarda otra vez lo que el usuario coloca
+    queue<int> predeterminado; // guarda otra vez lo que el usuario coloca
     stack<int> transicion; // guarda los elementos para transicion
-    stack<int>fin; // lista final
+    stack<int> fin; // lista final
     int veces;
     bool continuo=true;
     bool iguales;
     int num;
-    while(continuo){
-        cin>>veces;
-        if(veces == 0 && num== 0) {
-            continuo=false;
+    int contiwhi=true;
+    cin>>veces;
+    while(continuo) {
+        if(veces==0) {
+            cin>>veces;
+            if(veces==0) {
+                continuo=false;
+            } else {
+                de1aN=stack<int>();
+                acomodo=stack<int>();
+                predeterminado=queue<int>();
+                transicion=stack<int>();
+                fin=stack<int>();
+            }
         }
-        else{
-            for(int i=1; i<=veces;i++){
+        else if(veces!=0){
+            for(int i=1;i<=veces;i++) {
                 int num;
-                cin>>num;
+                cin >> num;
                 de1aN.push(i);
                 acomodo.push(num);
                 predeterminado.push(num);
-                if(!acomodo.empty() && !de1aN.empty() && de1aN.top()==acomodo.top()){
+            }
+            for(int i = 1; i <= veces; i++){
+                if(!acomodo.empty() && !de1aN.empty() && de1aN.top() == acomodo.top()) {
                     fin.push(acomodo.top());
                     de1aN.pop();
                     acomodo.pop();
-                }
-                else if (!acomodo.empty() && !de1aN.empty() && de1aN.top()!=acomodo.top()){
+                } else if (!acomodo.empty() && !de1aN.empty() && de1aN.top() != acomodo.top()) {
                     transicion.push(de1aN.top());
                     de1aN.pop();
                     acomodo.pop();
-                }
-                else if(!acomodo.empty() && !transicion.empty()&& acomodo.top()==transicion.top()){
+                } if(!acomodo.empty() && !transicion.empty() && acomodo.top() == transicion.top()) {
                     fin.push(transicion.top());
-                    de1aN.pop();
+                    transicion.pop();
                     acomodo.pop();
-                }
-                else if(!acomodo.empty() && !transicion.empty()&& acomodo.top()!=transicion.top()){
+                } else if(!acomodo.empty() && !transicion.empty() && acomodo.top() != transicion.top()) {
                     fin.push(transicion.top());
-                    de1aN.pop();
+                    transicion.pop();
                     acomodo.pop();
                 }
             }
             iguales=true;
-            for(int j=1;j<=fin.size();j++){
-                if(fin.top()==predeterminado.top()){
+            while(!fin.empty() && !predeterminado.empty() && contiwhi==true) {
+                if(fin.top()==predeterminado.front()) {
                     fin.pop();
                     predeterminado.pop();
                     iguales=true;
-                }
-                else if(fin.top()!=predeterminado.top()){
+                    contiwhi=true;
+                } else {
+                    iguales =false;
+                    contiwhi=false;
                     fin.pop();
                     predeterminado.pop();
-                    iguales=false;
                 }
             }
-            if(iguales==true){
+            if(iguales) {
                 cout<<"Yes"<<endl;
-            }
-            else if(iguales==false){
+            } else {
                 cout<<"No"<<endl;
             }
+            cin>>veces;
         }
-
         de1aN=stack<int>();
         acomodo=stack<int>();
-        predeterminado=stack<int>();
+        predeterminado=queue<int>();
         transicion=stack<int>();
         fin=stack<int>();
     }
